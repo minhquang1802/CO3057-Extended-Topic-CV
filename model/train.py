bce9bddf7535bd5f2cbf937dc3564d9c26ecf632
mwan <minhquang180203@gmail.com>
2024-11-08 22:55:07 +0700

    initial

---
+++ model/train.py
@@ -0,0 +1,31 @@
+from monai.networks.nets import UNet
+from monai.networks.layers import Norm
+from monai.losses import DiceLoss, DiceCELoss
+
+import torch
+from preporcess import prepare
+from utilities import train
+
+
+data_dir = 'D:/Books/DATN_LVTN/dataset/Task06_Lung/data_train_test'
+model_dir = 'D:/Books/DATN_LVTN/dataset/Task06_Lung/CV-Topic/Model/results' 
+data_in = prepare(data_dir, cache=True)
+
+device = torch.device("cuda:0")
+model = UNet(
+    spatial_dims=3,
+    in_channels=1,
+    out_channels=2,
+    channels=(16, 32, 64, 128, 256), 
+    strides=(2, 2, 2, 2),
+    num_res_units=2,
+    norm=Norm.BATCH,
+).to(device)
+
+
+#loss_function = DiceCELoss(to_onehot_y=True, sigmoid=True, squared_pred=True, ce_weight=calculate_weights(1792651250,2510860).to(device))
+loss_function = DiceLoss(to_onehot_y=True, sigmoid=True, squared_pred=True)
+optimizer = torch.optim.Adam(model.parameters(), 1e-4, weight_decay=1e-5, amsgrad=True)
+
+if __name__ == '__main__':
+    train(model, data_in, loss_function, optimizer, 600, model_dir)
